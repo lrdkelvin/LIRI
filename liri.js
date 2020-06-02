f7aea7b13bf5a90994f91f1a308fddb466ad98e9
lrdkelvin <brandonbringhurst@gmail.com>
2020-06-01 21:24:01 -0600

    added steps 2, 3, and working on fs recall.  Everything up to that point seems to be working

--- liri.js
+++ liri.js
@@ -2,31 +2,23 @@ require("dotenv").config();
 var moment = require('moment'); // require
 moment().format(); 
 const axios = require('axios');
+var fs = require("fs");
+
+//var spotification = new Spotify(keys.spotify);
 var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
 
 if (process.argv[2] == "concert-this") {
 
 var api1 = "https://rest.bandsintown.com/artists/";
-var artist = process.argv[3];
-
-if (process.argv[4] == undefined) {
-}  else {
-    artist = process.argv[3] + "+" + process.argv[4];
-}
-if (process.argv[5] == undefined) {
-}  else { 
-    artist = process.argv[3] + "+" + process.argv[4] + "+" + process.argv[5];
-}
-if (process.argv[6] == undefined) {
-}  else { 
-    artist = process.argv[3] + "+" + process.argv[4] + "+" + process.argv[5] + "+" + process.argv[6];
-}
+var input = process.argv[3];
 //process.argv[2];
 var api2 =  "/events?app_id=codingbootcamp";
 
 
-var query = (api1 + artist + api2);
+var query = (api1 + input + api2);
 axios.get(query)
   .then(function (response) {
     // handle success
@@ -48,22 +40,71 @@ axios.get(query)
     console.log("error occured" + error);
   })
 }//concert this if
-else {console.log("error: please specify valid command")} //end concert-this
 
-
-  //spotify API example
- /*
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+else if (process.argv[2] == "spotify-this-song") {
+    if (process.argv[3] == undefined) {
+        var input = "The Sign"
+    }
+    else {var input = process.argv[3]};
+   
  
-console.log(data); 
-});
+      spotify
+      .search({ type: 'track', query: input, limit: 1 })
+      .then(function(response) {
+          var artists = [];
+          var res = response.tracks.items[0];
+          
+        
+          for (b=0; b<res.artists.length; b++) {
+        artists.push(res.artists[b].name);
+          }
+          console.log("Artist(s): " + artists.join(" and "));
+          console.log("Song Title: " + res.name);
+          console.log("Link to Preview: " + res.preview_url);
+          console.log("Album: " + res.album.name);
+      })
+      .catch(function(err) {
+        if (err) {
+            throw err;
+        }
+      });
+}
+else if (process.argv[2] == "movie-this") {
+    var Omdb = "http://www.omdbapi.com/?i=tt3896198&apikey=35725016&t=";
+    if (process.argv[3] == undefined) {
+        var query = (Omdb + 'mr nobody');
+    }
+    else {
+    var query = (Omdb + process.argv[3]);
+    }
+    axios.get(query)
+  .then(function (response) {
+    // handle success
+    var res = response.data
+    console.log(res);
+    console.log("Title: " +res.Title);
+    console.log("Year released: " + res.Year);
+    console.log("IMDB Rating: " + res.imdbRating);
+    if (res.Ratings[1] == undefined) {
+    } else {
+    console.log("Rotten Tomatoes Rating: " + res.Ratings[1].Value);
+    };
+    console.log("Country: " + res.Country);
+    console.log("Language: " + res.Language);
+    console.log("Plot Summary: " + res.Plot);
+    console.log("Starring: " + res.Actors);
 
-// node liri.js concert this <artist/band name here>
-//this will search the Bands in Town ARtist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-//spotify-this
-
-//node liri.js spotify-this-song 
-*/
+  })
+  .catch(function (error) {
+    // handle error
+    console.log("error occured" + error);
+ })
+}
+else if (process.argv[2] = "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", (err, data) => {
+      if (!err) {
+          console.log(data);
+      }
+  })
+}
+else {console.log("error: please specify valid command")}
